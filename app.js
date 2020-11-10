166b1935a5761d72f4cafed6ae83af614d516c20
Adnan <adnan@rebeltechnology.io>
2020-11-10 15:00:00 +0500

    [fixed-bugs]: fixed bugs and updated code style

--- app.js
+++ app.js
@@ -1,60 +1,17 @@
-  
-       // const http = require('http');
-       var express = require('express');
-       var app = express();
-       var bodyParser = require('body-parser');
-       const logger = require('morgan');
-       const routes = require('./server/routes');
-        
-       
-       
-       //const hostname = '127.0.0.1';
-       //const hostname = 'localhost';
-        //const port = 3000;
-        //const server = http.createServer(app);
-        app.use(logger('dev'));
-        app.use(bodyParser.json());
-        app.use(bodyParser.urlencoded({ extended: true }));
+// importing required packages
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
 
-        routes(app);
+const app = express();
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-        app.get('*', (req, res) => res.status(200).send({
-          message: 'Welcome to the .',
-        }));
+app.get("*", (req, res) =>
+  res.status(200).send({
+    message: "Welcome to the .",
+  })
+);
 
-      /*  server.listen(port, hostname, () => {
-          console.log(`Server running at http://${hostname}:${port}/`);
-        });  */
-        
-       
-     /*  app.use(bodyParser.json());
-       app.use(bodyParser.urlencoded({
-           extended: true
-       }));  */
-       
-       
-
- var mysql = require('mysql');
-var con = mysql.createConnection({
-  host: "localhost",
-  //host: '127.0.0.1',
-  user: "root",
-  password: "root1234",
-  database: "bookstore"
-});
-
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  /*con.query("CREATE DATABASE bookstore", function (err, result) {
-    if (err) throw err;
-    console.log("Database created");
-  });*/
-});
-
-        
 module.exports = app;
-       
-
-
-
--- server/models/user.js
+++ server/models/user.js
@@ -1,55 +1,57 @@
-f1= (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: {
-        args: false,
-        msg: 'Please enter your name'
-      }
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: {
-        args: false,
-        msg: 'Please enter your username'
-      }
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: {
-        args: false,
-        msg: 'Please enter your email address'
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    "User",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: {
+          args: false,
+          msg: "Please enter your name",
+        },
       },
-      unique: {
-        args: true,
-        msg: 'Email already exists'
+      username: {
+        type: DataTypes.STRING,
+        allowNull: {
+          args: false,
+          msg: "Please enter your username",
+        },
       },
-      validate: {
-        isEmail: {
+      email: {
+        type: DataTypes.STRING,
+        allowNull: {
+          args: false,
+          msg: "Please enter your email address",
+        },
+        unique: {
           args: true,
-          msg: 'Please enter a valid email address'
+          msg: "Email already exists",
+        },
+        validate: {
+          isEmail: {
+            args: true,
+            msg: "Please enter a valid email address",
+          },
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: {
+          args: false,
+          msg: "Please enter a password",
+        },
+        validate: {
+          isNotShort: (value) => {
+            if (value.length < 5) {
+              throw new Error("Password should be at least 5 characters");
+            }
+          },
         },
       },
     },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: {
-        args: false,
-        msg: 'Please enter a password'
-      },
-      validate: {
-        isNotShort: (value) => {
-          if (value.length < 5) {
-            throw new Error('Password should be at least 5 characters');
-          }
-        },
-      },
-    }
-  }, {});
+    {}
+  );
   User.associate = (models) => {
     // associations can be defined here
   };
   return User;
 };
-
-module.exports = f1;
